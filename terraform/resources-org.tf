90b16e2f8e2592ae93fbdbda5a9e021c815ae210
cunla <cunla@users.noreply.github.com>
2025-12-10 00:49:37 +0000

    [AUTO]Format code
    Files changed:
    M	terraform/resources-org.tf

--- terraform/resources-org.tf
+++ terraform/resources-org.tf
@@ -27,7 +27,7 @@ resource "github_membership" "this" {
 
 resource "github_organization_role_team" "admins_security_manager" {
   team_slug = github_team.org_teams["Admins"].slug
-  role_id     = 138 #
+  role_id   = 138 #
 }
 
 # Create the organization teams for Django Commons.

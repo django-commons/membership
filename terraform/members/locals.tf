17273e3a87ebd830630546f3b2a88e5b1cea1f96
Daniel M <daniel.maruani@gmail.com>
2025-12-18 16:11:01 -0500

    separate modules

--- terraform/members/locals.tf
+++ terraform/members/locals.tf
@@ -1,10 +1,3 @@
-# Local Values
-# https://www.terraform.io/language/values/locals
-
 locals {
 
-  project_repositories = {
-    for repository_key, repository in var.repositories : repository_key => repository
-    if !repository.is_django_commons_repo
-  }
 }
